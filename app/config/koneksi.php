67b72770b80ab5fead1d88a3d176dacc9be3a76b
gema_handika <gemahandika88@gmail.com>
2024-12-27 15:24:11 +0700

    koneksi

--- app/config/koneksi.php
+++ app/config/koneksi.php
@@ -1,7 +1,13 @@
 
 <?php
 
-// koneksi database
+// koneksi local
+// $koneksi = mysqli_connect('localhost', 'root', '', 'jnee6778_itbase');
+// if (mysqli_connect_errno()) {
+//     echo mysqli_connect_error();
+// }
+
+// koneksi public
 $koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_itbase');
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
